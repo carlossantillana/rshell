0c002368f41ed005d28566999f5da247ac746bb6
Carlos Santillana <csant019@ucr.edu>
2017-05-01 16:52:20 -0700

    added semicolon

---
+++ semicolon.h
@@ -0,0 +1,29 @@
+#ifndef __SEMICOLON_H__
+#define __SEMICOLON_H__
+
+#include "constructor.h"
+
+class SemiColon : public Connector
+{
+private:
+	RShell *left;
+	RShell *right;
+public:
+	SemiColon(RShell *l, RShell *r) //Constructor
+	: left(l}, right(r)
+	{}
+
+	bool execute() //Returns true if one argument is true
+	{
+		if(left->execute() || right->execute())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
+
+#endif
