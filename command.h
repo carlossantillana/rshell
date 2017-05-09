88d308631c29165d1bd74482f20b2619fe8383f0
Jonathan Woolf <jwool003@ucr.edu>
2017-05-08 21:12:22 -0700

    Minor command fixes and comments

--- command.h
+++ command.h
@@ -1,25 +1,25 @@
 #ifndef __COMMAND_H__
 #define __COMMAND_H__
 
-#include "execution.h"
+#include "rshell.h"
 
-class Command: public Execution
+class Command: public RShell
 {
     private:
-        string type;
+        string type; //Ex. ls, ls -a (Don't know what else should work TBH)
     public:
-        Command(string i)
+        Command(string i) //Constructor reads in command type
         :type(i)
         {}
-        virtual bool execute()
+        virtual bool execute() //Returns true if command exists
         {
           return true;
         }
-        virtual string get_input()
+        virtual string get_input() //Not really doing much
         {
           return "";
         }
-        string get_type()
+        string get_type() //Returns type for rshell comparisons
         {
           return type;
         }
